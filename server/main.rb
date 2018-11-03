509f3161476f0fe49739f7ed5b6e68c9dd5c3e4c
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-11-03 17:02:18 -0500

    Move zapier stuff into code

--- server/main.rb
+++ server/main.rb
@@ -1,14 +1,43 @@
 require "json"
 require "pg"
+require "twilio-ruby"
 require "sequel"
 require "sinatra"
 
 DB = Sequel.connect ENV.fetch("DATABASE_URL")
 AUTH_SECRET = ENV.fetch("AUTH_SECRET")
+TWILIO = Twilio::REST::Client.new(
+  ENV.fetch("TWILIO_SID"), ENV.fetch("TWILIO_AUTH_TOKEN"))
+TWILIO_NUMBER = ENV.fetch("TWILIO_NUMBER")
+
+def ok
+  return {}
+end
+
+def json
+  JSON.load(request.body)
+end
+
+def authorize!
+  raise unless env["HTTP_AUTHORIZATION"] == AUTH_SECRET
+end
+
+def has_keywords(tweet)
+  tweet =~ /shot|shoot/ && tweet =~ /die|dead/
+end
+
+def new_crime!
+  DB[:crime].insert(tweet: tweet, date: Time.now)
+  TWILIO.api.account.messages.create(
+    from: TWILIO_NUMBER,
+    to: DB[:userinfo].select_map(:pnum).join(","),
+    body: "Pray at noticechicago.com!\n\n" + tweet
+  )
+end
 
 post "/incident" do
-  raise unless env["HTTP_AUTHORIZATION"] == AUTH_SECRET
-  tweet = JSON.load(request.body).fetch("tweet")
-  DB[:crime].insert(tweet: tweet, date: Time.now)
-  return {}
+  authorize!
+  tweet = json.fetch "tweet"
+  new_crime! if has_keywords tweet
+  ok
 end
