18ea5dc8926c4c363a01077d671b6586282fdcd9
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-02-24 20:58:37 -0600

    Add new endpoint for graph plot

--- server/main.rb
+++ server/main.rb
@@ -6,6 +6,8 @@ require "sinatra"
 require_relative "api"
 
 DB = Sequel.connect ENV.fetch("DATABASE_URL")
+DB.extension :pg_json
+
 TWILIO = Twilio::REST::Client.new(
   ENV.fetch("TWILIO_SID"), ENV.fetch("TWILIO_AUTH_TOKEN"))
 TWILIO_NUMBER = ENV.fetch("TWILIO_NUMBER")
@@ -36,9 +38,9 @@ def last_five_days(table)
     FULL JOIN (
       SELECT column1 AS date, 0 AS zero FROM (
       VALUES
-        (CURRENT_DATE), 
-        (CURRENT_DATE + INTERVAL '-1 day'), 
-        (CURRENT_DATE + INTERVAL '-2 day'), 
+        (CURRENT_DATE),
+        (CURRENT_DATE + INTERVAL '-1 day'),
+        (CURRENT_DATE + INTERVAL '-2 day'),
         (CURRENT_DATE + INTERVAL '-3 day'),
         (CURRENT_DATE + INTERVAL '-4 day')
       ) x
@@ -48,10 +50,45 @@ def last_five_days(table)
   SQL
 end
 
+def crime_nodes
+  <<-SQL
+    SELECT
+      'CRIME'                     AS type,
+      TO_CHAR(date, 'YYYY-MM-DD') AS label,
+      TO_JSON(ARRAY_AGG(tweet))   AS tweets
+    FROM crime
+    GROUP by date;
+  SQL
+end
+
+def prayer_nodes
+  <<-SQL
+    SELECT
+      'PRAYER'                    AS type,
+      TO_CHAR(date, 'YYYY-MM-DD') AS label
+    FROM prayer;
+  SQL
+end
+
 def get_plot_stuff
   {
     "Prayer": DB[last_five_days "prayer"].select_map(:count),
     "Gunshot Fatalities": DB[last_five_days "crime"].select_map(:count),
+  }
+end
+
+def with_ids(start, query)
+  query.to_enum.with_index(start).map { |x,i| x[:id] = i; x }
+end
+
+def get_graph_stuff
+  crime = with_ids(0, DB[crime_nodes])
+  prayer = with_ids(crime.count, DB[prayer_nodes])
+  {
+    "Nodes": crime + prayer,
+    "Edges": prayer.map do |p|
+      crime.map { |c| { from: p[:id], to: c[:id] } if p[:label] == c[:label] }.compact
+    end
   }
 end
 
@@ -69,4 +106,8 @@ end
 
 get "/plot/last-week" do
   Api.ok get_plot_stuff
+end
+
+get "/plot/graph" do
+  Api.ok get_graph_stuff
 end
